471fd1d43dcc2db334d70a88c2d5f8c17e728102
crewative-builder <saydivakasovm@gmail.com>
2025-11-22 17:27:25 +0500

    Final Fix: Moved maplibre CSS import to src/index.css for stable PostCSS/Vite build.

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// ADD the MapLibre CSS import here:
-import "maplibregl/dist/maplibre-gl.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
